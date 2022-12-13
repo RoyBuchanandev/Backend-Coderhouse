e34b5662eab573a45bd8010414f720e0064e020f
RoyBuchanandev <93665139+RoyBuchanandev@users.noreply.github.com>
2022-12-13 12:42:09 -0300

    Add files via upload

--- DesafioPoo.js
+++ DesafioPoo.js
@@ -1,41 +1,30 @@
-// desafio clases --> realizar una clase ProductManager que gestione un conjunto de productos.
-// debe crearse desde su constructor con un elemento products que es un array vacio
-// cada producto que gestione dicho array tiene que tener propiedades(title,description,price,thumbnail,code,stock)
-// Metodos: 
-// addProducts --> al agregar un producto al array (el agregarlo tiene que crearse un id que se incremente, validar que no se repita el id del producto)
-// getProducts --> devolver el array con todos los productos
-// getProductById --> buscar en el array con find un id, si lo encuentra devuelve el producto sino tira "Not found"
-
-
 class ProductManager {
     static counter = 0 // declaro contador de manera static (global)
-    static product = []; // declaro product como array vacio de manera static (global)
-    constructor(title, description, price, thumbnail, stock) {
+    constructor(title, description, price, thumbnail, code, stock) {
         this.product = [];
         this.title = title;
         this.description = description;
         this.price = price;
         this.thumbnail = thumbnail;
         this.stock = stock
+        this.code = code;
         this.id = ProductManager.counter += 1; // redefino valor de counter como id para que sea mas facil de interpretar 
     }
 
-    addProducts() { // metodo para pushear todos los campos dentro del array vacio product + id autoincrementado + validacion de no repetir id + validacion campos empty
+    addProducts() { // metodo para pushear todos los campos dentro del array vacio product + id autoincrementado + validacion de no repetir id 
         this.id + 1;
-        let idAsignado = this.product.find((product) => product.id === id)
-        if (this.title.length === 0 || this.description.length === 0 || this.price.length === 0 || this.thumbnail.length === 0 || this.stock.length === 0) {
-            return ('es necesario llenar todos los campos')
-        } if (idAsignado) {
-            console.log('el id se repite');
-        } else {
+        if (this.title !== undefined || this.description !== undefined || this.price !== undefined || this.thumbnail !== undefined || this.code !== undefined || this.stock !== undefined) {
             return this.product.push(this.title, this.description, this.price, this.thumbnail, this.stock)
-                && ProductManager.product.push(this.title, this.description, this.price, this.thumbnail, this.stock)
+        } else if ((this.product.find(p => p.id === product.id))) {
+            throw new Error('El código del producto ya existe')
+        } else {
+            throw new Error('Todos los campos son obligatorios');
         }
-
     }
 
+
     getProducts() { // retono los productos que pushee en el array static global y los muestro
-        return [...ProductManager.product] // utilizo spread operator para retornar lo que contiene el array
+        return [...this.product] // utilizo spread operator para retornar lo que contiene el array
     }
 
 
@@ -45,9 +34,12 @@ class ProductManager {
 
 }
 
-const productoUno = new ProductManager('Nombre: Kimono Atama', 'Descripcion: A1 negro', 27000, 'thumbnail', 20)
-console.log(productoUno.addProducts(), productoUno.getProducts(), productoUno.getProductsById());
-
-
-const productoDos = new ProductManager('Nombre: Kimono shoyoroll', 'Descripcion: A1 negro', 100000, 'thumbnail', 30)
-console.log(productoDos.addProducts(), productoDos.getProducts(), productoDos.getProductsById());
+// Instanciamos la clase
+const productManager = new ProductManager(
+    "Kimono kingz", // Título del producto
+    "A1 negro", // Descripción del producto
+    100.0, // Precio del producto
+    "Kimonokingz.jpg", // Ruta de la imagen del producto
+    "43564", // Código identificador del producto
+    5 // Número de piezas disponibles
+);
